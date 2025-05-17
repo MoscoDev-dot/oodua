b96851a8d586ef33d16710012688f674dd7ff337
mosco <sajuyigbemoses@gmail.com>
2025-05-17 02:26:29 +0100

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { Routes, Route } from 'react-router-dom'
+
 import Home from './Navlinks/Home'
-import AboutIfa from './Navlinks/AboutIfa'
-import ExploreVerses from './Navlinks/ExploreVerses'
-import Features from './Navlinks/Features'
-import Pricing from './Navlinks/Pricing'
-import Login from './Navlinks/Login'
+
 
 
 
@@ -15,14 +11,7 @@ const App = () => {
     <div>
       
     <Navbar />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<AboutIfa />} />
-      <Route path="/explore" element={<ExploreVerses />} />
-      <Route path="/features" element={<Features />} />
-      <Route path="/pricing" element={<Pricing />} />
-      <Route path="/login" element={<Login />} />
-    </Routes>
+    
     <Home />
     
     </div>
